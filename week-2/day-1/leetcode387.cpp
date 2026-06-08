6e992bcded3c0726f2168ed0c119f109f8beaf31
devanshbhadauriya000-max <devanshbhadauriya000@gmail.com>
2026-06-08 14:06:39 +0530

    week-2

---
+++ week-2/day-1/leetcode387.cpp
@@ -0,0 +1,13 @@
+unordered_map<char, int> freq;
+    for(char ch : s) {
+        freq[ch]++;
+    }
+
+    for(int i = 0; i < s.size(); i++) {
+
+            if(freq[s[i]] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
