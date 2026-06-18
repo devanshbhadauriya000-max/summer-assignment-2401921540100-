dbc562ab0046c18c274133b2ac2e31640f79ac3d
devanshbhadauriya000-max <devanshbhadauriya000@gmail.com>
2026-06-18 14:08:15 +0530

    Create leetcode496.cpp

---
+++ week 3/day 3/leetcode496.cpp
@@ -0,0 +1,30 @@
+ unordered_map<int  ,  int> map; //  nums2[i] , NG
+             stack<int> s;
+
+             for(int  i=nums2.size()-1 ; i>=0 ; i--)
+             {
+                while(s.size() > 0 && s.top() <= nums2[i])
+                {
+                    s.pop();
+                }
+
+                if(s.empty())
+                {
+                  map[nums2[i]] = -1;
+                }
+
+                else
+                {
+                    map[nums2[i]] = s.top();
+                }
+
+                s.push(nums2[i]);
+             }
+
+             vector<int> ans;
+             for(int i=0 ; i<nums1.size() ; i++)
+             {
+                ans.push_back(map[nums1[i]]);
+             }
+
+             return ans;
