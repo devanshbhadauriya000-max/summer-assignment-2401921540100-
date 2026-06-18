8d842031dc3e82c41f43f2a7b528435a7dc0c501
devanshbhadauriya000-max <devanshbhadauriya000@gmail.com>
2026-06-18 18:14:24 +0530

    Create leetcode739.cpp

---
+++ week 3/day 4/leetcode739.cpp
@@ -0,0 +1,19 @@
+
+  stack<int>s;
+        int n = temperatures.size();
+        vector<int>ans(n,0);
+
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            while(!s.empty() && temperatures[s.top()] <= temperatures[i])
+            s.pop();
+
+            if(!s.empty()){
+                ans[i] = s.top() - i;
+            }
+
+            s.push(i);
+        }
+
+        return ans;
+        
